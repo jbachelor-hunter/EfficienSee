07d61ec1a9e977e2c3358bd85d4a0c9be21ecf5c
Jonathan Bachelor <jon_bachelor@me.com>
2017-05-13 11:42:58 -0700

    Adding a value converter to beautify the result displayed.

---
+++ EfficienSee/Views/ValueConverters/IntSecondsToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EfficienSee.Views.ValueConverters
+{
+    public class IntSecondsToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int seconds;
+            string display = string.Empty;
+
+            if (!Int32.TryParse(value.ToString(), out seconds))
+            {
+                display = "???";
+            }
+            else
+            {
+                display = $"{seconds:n} seconds";
+            }
+
+            return display;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

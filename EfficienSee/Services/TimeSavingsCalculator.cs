b80e591dae4bdb5f897d4b466178b8c4fc241e30
Jonathan Bachelor <jon_bachelor@me.com>
2017-05-17 12:43:28 -0700

    Fixed bug where app would crash if you didn’t have task lifetime units selected. Set max minutes shaved to 120.

--- EfficienSee/Services/TimeSavingsCalculator.cs
+++ EfficienSee/Services/TimeSavingsCalculator.cs
@@ -6,98 +6,103 @@ using EfficienSee.Infrastructure;
 [assembly: InternalsVisibleTo("EfficienSeeTests")]
 namespace EfficienSee.Services
 {
-    public class TimeSavingsCalculator : ITimeSavingsCalculator
-    {
-        /// <summary>
-        /// Calculates the amount of time that would be saved by making a task more efficient in some way.
-        /// </summary>
-        /// <param name="timeSavedPerTask">How much time one could shorten a task by</param>
-        /// <param name="taskFrequency">How frequently one engages in the task</param>
-        /// <param name="taskFrequencyUnitOfTime">day/week/month/year</param>
-        /// <param name="taskLifetime">How long will the task go on in days/weeks/months/years</param>
-        /// <param name="taskLifetimeUnitOfTime">day/week/month/year</param>
-        /// <returns></returns>
-        public TimeSpan GetMaxTimeSaved(TimeSpan timeSavedPerTask, int taskFrequency, string taskFrequencyUnitOfTime, int taskLifetime, string taskLifetimeUnitOfTime)
-        {
-            double secondsSavedPerFrequencyUnitOfTime = timeSavedPerTask.TotalSeconds * taskFrequency;
-            double taskLifetimeInFrequencyUnitOfTime = GetTaskLifetimeInTaskFrequencyUnitOfTime(
-                taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, taskLifetime);
+	public class TimeSavingsCalculator : ITimeSavingsCalculator
+	{
+		/// <summary>
+		/// Calculates the amount of time that would be saved by making a task more efficient in some way.
+		/// </summary>
+		/// <param name="timeSavedPerTask">How much time one could shorten a task by</param>
+		/// <param name="taskFrequency">How frequently one engages in the task</param>
+		/// <param name="taskFrequencyUnitOfTime">day/week/month/year</param>
+		/// <param name="taskLifetime">How long will the task go on in days/weeks/months/years</param>
+		/// <param name="taskLifetimeUnitOfTime">day/week/month/year</param>
+		/// <returns></returns>
+		public TimeSpan GetMaxTimeSaved(TimeSpan timeSavedPerTask, int taskFrequency, string taskFrequencyUnitOfTime, int taskLifetime, string taskLifetimeUnitOfTime)
+		{
+			double secondsSavedPerFrequencyUnitOfTime = timeSavedPerTask.TotalSeconds * taskFrequency;
+			double taskLifetimeInFrequencyUnitOfTime = GetTaskLifetimeInTaskFrequencyUnitOfTime(
+				taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, taskLifetime);
 
-            var maxTimeSavedInSeconds = secondsSavedPerFrequencyUnitOfTime * taskLifetimeInFrequencyUnitOfTime;
-            return TimeSpan.FromSeconds(maxTimeSavedInSeconds);
-        }
+			var maxTimeSavedInSeconds = secondsSavedPerFrequencyUnitOfTime * taskLifetimeInFrequencyUnitOfTime;
+			return TimeSpan.FromSeconds(maxTimeSavedInSeconds);
+		}
 
-        /// <summary>
-        /// Converts task lifetime to be in the same unit of time that task frequency is measured in.
-        /// </summary>
-        /// <param name="taskFrequencyUnitOfTime">day/week/month/year</param>
-        /// <param name="taskLifetimeUnitOfTime">day/week/month/year</param>
-        /// <param name="taskLifetime">How long will the task go on in days/weeks/months/years</param>
-        /// <returns></returns>
-        internal double GetTaskLifetimeInTaskFrequencyUnitOfTime(string taskFrequencyUnitOfTime, string taskLifetimeUnitOfTime, int taskLifetime)
-        {
-            double taskLifetimeInFrequencyUnitOfTime = 0.0;
+		/// <summary>
+		/// Converts task lifetime to be in the same unit of time that task frequency is measured in.
+		/// </summary>
+		/// <param name="taskFrequencyUnitOfTime">day/week/month/year</param>
+		/// <param name="taskLifetimeUnitOfTime">day/week/month/year</param>
+		/// <param name="taskLifetime">How long will the task go on in days/weeks/months/years</param>
+		/// <returns></returns>
+		internal double GetTaskLifetimeInTaskFrequencyUnitOfTime(string taskFrequencyUnitOfTime, string taskLifetimeUnitOfTime, int taskLifetime)
+		{
+			if (string.IsNullOrWhiteSpace(taskFrequencyUnitOfTime) || string.IsNullOrWhiteSpace(taskLifetimeUnitOfTime))
+			{
+				return 0.0;
+			}
 
-            if (string.Equals(taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, StringComparison.OrdinalIgnoreCase))
-            {
-                taskLifetimeInFrequencyUnitOfTime = taskLifetime;
-            }
-            else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("week"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetDaysFromWeeks(taskLifetime);
-            }
-            else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("month"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetDaysFromMonths(taskLifetime);
-            }
-            else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("year"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetDaysFromYears(taskLifetime);
-            }
-            else if (taskFrequencyUnitOfTime.Contains("week") && taskLifetimeUnitOfTime.Contains("month"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetWeeksFromMonths(taskLifetime);
-            }
-            else if (taskFrequencyUnitOfTime.Contains("week") && taskLifetimeUnitOfTime.Contains("year"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetWeeksFromYears(taskLifetime);
-            }
-            else if (taskFrequencyUnitOfTime.Contains("month") && taskLifetimeUnitOfTime.Contains("year"))
-            {
-                taskLifetimeInFrequencyUnitOfTime = GetMonthsFromYears(taskLifetime);
-            }
+			double taskLifetimeInFrequencyUnitOfTime = 0.0;
 
-            return taskLifetimeInFrequencyUnitOfTime;
-        }
-        
-        internal int GetDaysFromYears(int years)
-        {
-            return years * Globals.DaysPerYear;
-        }
+			if (string.Equals(taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, StringComparison.OrdinalIgnoreCase))
+			{
+				taskLifetimeInFrequencyUnitOfTime = taskLifetime;
+			}
+			else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("week"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetDaysFromWeeks(taskLifetime);
+			}
+			else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("month"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetDaysFromMonths(taskLifetime);
+			}
+			else if (taskFrequencyUnitOfTime.Contains("day") && taskLifetimeUnitOfTime.Contains("year"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetDaysFromYears(taskLifetime);
+			}
+			else if (taskFrequencyUnitOfTime.Contains("week") && taskLifetimeUnitOfTime.Contains("month"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetWeeksFromMonths(taskLifetime);
+			}
+			else if (taskFrequencyUnitOfTime.Contains("week") && taskLifetimeUnitOfTime.Contains("year"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetWeeksFromYears(taskLifetime);
+			}
+			else if (taskFrequencyUnitOfTime.Contains("month") && taskLifetimeUnitOfTime.Contains("year"))
+			{
+				taskLifetimeInFrequencyUnitOfTime = GetMonthsFromYears(taskLifetime);
+			}
 
-        internal double GetDaysFromMonths(int months)
-        {
-            return months * Globals.DaysPerMonth;
-        }
+			return taskLifetimeInFrequencyUnitOfTime;
+		}
 
-        internal double GetDaysFromWeeks(int weeks)
-        {
-            return weeks * Globals.DaysPerWeek;
-        }
+		internal int GetDaysFromYears(int years)
+		{
+			return years * Globals.DaysPerYear;
+		}
 
-        internal double GetWeeksFromYears(int years)
-        {
-            return years * Globals.WeeksPerYear;
-        }
+		internal double GetDaysFromMonths(int months)
+		{
+			return months * Globals.DaysPerMonth;
+		}
 
-        internal double GetWeeksFromMonths(int months)
-        {
-            return months * Globals.WeeksPerMonth;
-        }
+		internal double GetDaysFromWeeks(int weeks)
+		{
+			return weeks * Globals.DaysPerWeek;
+		}
 
-        internal double GetMonthsFromYears(int years)
-        {
-            return years * Globals.MonthsPerYear;
-        }
-    }
+		internal double GetWeeksFromYears(int years)
+		{
+			return years * Globals.WeeksPerYear;
+		}
+
+		internal double GetWeeksFromMonths(int months)
+		{
+			return months * Globals.WeeksPerMonth;
+		}
+
+		internal double GetMonthsFromYears(int years)
+		{
+			return years * Globals.MonthsPerYear;
+		}
+	}
 }
--- EfficienSeeTests/Services/TimeSavingsCalculatorTests.cs
+++ EfficienSeeTests/Services/TimeSavingsCalculatorTests.cs
@@ -5,132 +5,139 @@ using EfficienSee.Infrastructure;
 
 namespace EfficienSeeTests.Services
 {
-    [TestFixture()]
-    public class TimeSavingsCalculatorTests
-    {
-        TimeSavingsCalculator timeSavingsCalculator;
+	[TestFixture()]
+	public class TimeSavingsCalculatorTests
+	{
+		TimeSavingsCalculator timeSavingsCalculator;
 
-        [TestFixtureSetUp]
-        public void ClassInit()
-        {
+		[TestFixtureSetUp]
+		public void ClassInit()
+		{
 
-        }
+		}
 
-        [SetUp]
-        public void TestInit()
-        {
-            timeSavingsCalculator = new TimeSavingsCalculator();
-        }
-        
-        [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(10)]
-        public void TestGetDaysFromYears(int years)
-        {
-            var expectedResult = years * Globals.DaysPerYear;
-            var actualResult = timeSavingsCalculator.GetDaysFromYears(years);
-            Assert.AreEqual(expectedResult, actualResult);
-        }
+		[SetUp]
+		public void TestInit()
+		{
+			timeSavingsCalculator = new TimeSavingsCalculator();
+		}
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(10)]
-        public void TestGetWeekssFromYears(int years)
-        {
-            var expectedResult = years * Globals.WeeksPerYear;
-            var actualResult = timeSavingsCalculator.GetWeeksFromYears(years);
-            Assert.AreEqual(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(10)]
+		public void TestGetDaysFromYears(int years)
+		{
+			var expectedResult = years * Globals.DaysPerYear;
+			var actualResult = timeSavingsCalculator.GetDaysFromYears(years);
+			Assert.AreEqual(expectedResult, actualResult);
+		}
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(10)]
-        public void TestGetMonthsFromYears(int years)
-        {
-            var expectedResult = years * Globals.MonthsPerYear;
-            var actualResult = timeSavingsCalculator.GetMonthsFromYears(years);
-            Assert.AreEqual(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(10)]
+		public void TestGetWeekssFromYears(int years)
+		{
+			var expectedResult = years * Globals.WeeksPerYear;
+			var actualResult = timeSavingsCalculator.GetWeeksFromYears(years);
+			Assert.AreEqual(expectedResult, actualResult);
+		}
+
+		[Test]
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(10)]
+		public void TestGetMonthsFromYears(int years)
+		{
+			var expectedResult = years * Globals.MonthsPerYear;
+			var actualResult = timeSavingsCalculator.GetMonthsFromYears(years);
+			Assert.AreEqual(expectedResult, actualResult);
+		}
 
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(10)]
-        public void TestGetDaysFromMonths(int months)
-        {
-            var expectedResult = months * Globals.DaysPerMonth;
-            var actualResult = timeSavingsCalculator.GetDaysFromMonths(months);
-            TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase(1)]
+		[TestCase(2)]
+		[TestCase(10)]
+		public void TestGetDaysFromMonths(int months)
+		{
+			var expectedResult = months * Globals.DaysPerMonth;
+			var actualResult = timeSavingsCalculator.GetDaysFromMonths(months);
+			TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
+		}
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(4)]
-        [TestCase(10)]
-        public void TestGetWeeksFromMonths(int months)
-        {
-            var expectedResult = months * Globals.WeeksPerMonth;
-            var actualResult = timeSavingsCalculator.GetWeeksFromMonths(months);
-            TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase(1)]
+		[TestCase(4)]
+		[TestCase(10)]
+		public void TestGetWeeksFromMonths(int months)
+		{
+			var expectedResult = months * Globals.WeeksPerMonth;
+			var actualResult = timeSavingsCalculator.GetWeeksFromMonths(months);
+			TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
+		}
 
-        [Test]
-        [TestCase(1)]
-        [TestCase(4)]
-        [TestCase(10)]
-        public void TestGetDaysFromWeeks(int weeks)
-        {
-            var expectedResult = weeks * Globals.DaysPerWeek;
-            var actualResult = timeSavingsCalculator.GetDaysFromWeeks(weeks);
-            TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase(1)]
+		[TestCase(4)]
+		[TestCase(10)]
+		public void TestGetDaysFromWeeks(int weeks)
+		{
+			var expectedResult = weeks * Globals.DaysPerWeek;
+			var actualResult = timeSavingsCalculator.GetDaysFromWeeks(weeks);
+			TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
+		}
 
-        [Test]
-        [TestCase("week", "week", 8, 8)]
-        [TestCase("day", "day", 3, 3)]
-        [TestCase("month", "month", 5, 5)]
-        [TestCase("year", "year", 9, 9)]
-        [TestCase("week", "year", 1, 52.0)]
-        [TestCase("week", "year", 5, 260.0)]
-        [TestCase("week", "year", 10, 520.0)]
-        [TestCase("week", "month", 1, (Globals.WeeksPerMonth * 1))]
-        [TestCase("week", "month", 8, (Globals.WeeksPerMonth * 8))]      
-        [TestCase("month", "year", 1, (Globals.MonthsPerYear * 1))]
-        [TestCase("month", "year", 31, (Globals.MonthsPerYear * 31))]
-        [TestCase("day", "week", 1, 7.0)]
-        [TestCase("day", "week", 11, 77.0)]
-        [TestCase("day", "month", 1, (Globals.DaysPerMonth * 1))]
-        [TestCase("day", "month", 12, (Globals.DaysPerMonth * 12))]
-        [TestCase("day", "year", 1, (Globals.DaysPerYear * 1))]
-        [TestCase("day", "year", 18, (Globals.DaysPerYear * 18))]
-        public void TestGetTaskLifetimeInTaskFrequencyUnitOfTimeReturnsCorrectResult(string taskFrequencyUnitOfTime, string taskLifetimeUnitOfTime, int taskLifetime, double expectedResult)
-        {
-            var actualResult = timeSavingsCalculator.GetTaskLifetimeInTaskFrequencyUnitOfTime(taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, taskLifetime);
-            TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
-        }
+		[Test]
+		[TestCase("week", "week", 8, 8)]
+		[TestCase("day", "day", 3, 3)]
+		[TestCase("month", "month", 5, 5)]
+		[TestCase("year", "year", 9, 9)]
+		[TestCase("week", "year", 1, 52.0)]
+		[TestCase("week", "year", 5, 260.0)]
+		[TestCase("week", "year", 10, 520.0)]
+		[TestCase("week", "month", 1, (Globals.WeeksPerMonth * 1))]
+		[TestCase("week", "month", 8, (Globals.WeeksPerMonth * 8))]
+		[TestCase("month", "year", 1, (Globals.MonthsPerYear * 1))]
+		[TestCase("month", "year", 31, (Globals.MonthsPerYear * 31))]
+		[TestCase("day", "week", 1, 7.0)]
+		[TestCase("day", "week", 11, 77.0)]
+		[TestCase("day", "month", 1, (Globals.DaysPerMonth * 1))]
+		[TestCase("day", "month", 12, (Globals.DaysPerMonth * 12))]
+		[TestCase("day", "year", 1, (Globals.DaysPerYear * 1))]
+		[TestCase("day", "year", 18, (Globals.DaysPerYear * 18))]
+		public void TestGetTaskLifetimeInTaskFrequencyUnitOfTimeReturnsCorrectResult(string taskFrequencyUnitOfTime, string taskLifetimeUnitOfTime, int taskLifetime, double expectedResult)
+		{
+			var actualResult = timeSavingsCalculator.GetTaskLifetimeInTaskFrequencyUnitOfTime(taskFrequencyUnitOfTime, taskLifetimeUnitOfTime, taskLifetime);
+			TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult, actualResult);
+		}
 
-        [Test]
-        [TestCase(60, 10, "day", 1, "year", 219000)]
-        [TestCase(3600, 1, "day", 1, "year", 1314000)]
-        [TestCase(30, 4, "day", 1, "year", 43800)]
-        [TestCase(60, 4, "day", 1, "week", 1684.615)]
-        [TestCase(60, 4, "day", 7, "week", 11792.308)]
-        [TestCase(900, 3, "week", 1, "year", 140400)]
-        [TestCase(900, 3, "week", 5, "year", 702000)]
-        [TestCase(60, 5, "week", 1, "month", 1300)]
-        [TestCase(900, 10, "month", 1, "year", 108000)]
-        public void TestGetTotalTimeSavedForTaskRules(int secondsSavedPerTask, int taskFreq, string taskFreqUnitOfTime, int taskLifetime, string taskLifetimeUnitOfTime, double expectedMaxSecondsSaved)
-        {
-            var timeSavedPerTask = TimeSpan.FromSeconds(secondsSavedPerTask);
-            var expectedResult = TimeSpan.FromSeconds(expectedMaxSecondsSaved);
+		[Test]
+		public void GetTaskLifetimeInTaskFrequencyUnitOfTimeReturns0ForNulls()
+		{
+			var actualResult = timeSavingsCalculator.GetTaskLifetimeInTaskFrequencyUnitOfTime(null, null, 90);
+			Assert.AreEqual(0, actualResult);
+		}
 
-            var actualResult = timeSavingsCalculator.GetMaxTimeSaved(timeSavedPerTask, taskFreq, taskFreqUnitOfTime, taskLifetime, taskLifetimeUnitOfTime);
+		[Test]
+		[TestCase(60, 10, "day", 1, "year", 219000)]
+		[TestCase(3600, 1, "day", 1, "year", 1314000)]
+		[TestCase(30, 4, "day", 1, "year", 43800)]
+		[TestCase(60, 4, "day", 1, "week", 1684.615)]
+		[TestCase(60, 4, "day", 7, "week", 11792.308)]
+		[TestCase(900, 3, "week", 1, "year", 140400)]
+		[TestCase(900, 3, "week", 5, "year", 702000)]
+		[TestCase(60, 5, "week", 1, "month", 1300)]
+		[TestCase(900, 10, "month", 1, "year", 108000)]
+		public void TestGetTotalTimeSavedForTaskRules(int secondsSavedPerTask, int taskFreq, string taskFreqUnitOfTime, int taskLifetime, string taskLifetimeUnitOfTime, double expectedMaxSecondsSaved)
+		{
+			var timeSavedPerTask = TimeSpan.FromSeconds(secondsSavedPerTask);
+			var expectedResult = TimeSpan.FromSeconds(expectedMaxSecondsSaved);
 
-            TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult.TotalSeconds, actualResult.TotalSeconds);
-        }
-    }
+			var actualResult = timeSavingsCalculator.GetMaxTimeSaved(timeSavedPerTask, taskFreq, taskFreqUnitOfTime, taskLifetime, taskLifetimeUnitOfTime);
+
+			TestUtilities.AssertTwoDoublesAreCloseEnough(expectedResult.TotalSeconds, actualResult.TotalSeconds);
+		}
+	}
 }

fb0525e2d78b789e44cd72f78d9f1543ac9d509f
Jonathan Bachelor <jon_bachelor@me.com>
2017-05-18 08:31:06 -0700

    Updated usage of minutes as double to TimeSpan to hold the max time to allot data. Changing from displaying only minutes to a more readable format using TimeSpan.

--- EfficienSee/ViewModels/MainPageViewModel.cs
+++ EfficienSee/ViewModels/MainPageViewModel.cs
@@ -10,169 +10,180 @@ using System.Collections.ObjectModel;
 
 namespace EfficienSee.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigatingAware, IDestructible
-    {
-        private ITimeSavingsCalculator _timeSavingsCalculator;
+	public class MainPageViewModel : BindableBase, INavigatingAware, IDestructible
+	{
+		private ITimeSavingsCalculator _timeSavingsCalculator;
 
-        private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set { SetProperty(ref _title, value); }
+		}
 
-        private string _timeSavedLabelText;
-        public string TimeSavedLabelText
-        {
-            get { return _timeSavedLabelText; }
-            set { SetProperty(ref _timeSavedLabelText, value); }
-        }
+		private string _timeSavedLabelText;
+		public string TimeSavedLabelText
+		{
+			get { return _timeSavedLabelText; }
+			set { SetProperty(ref _timeSavedLabelText, value); }
+		}
 
-        private int _timeSavedPerTask;
-        public int TimeSavedPerTask
-        {
-            get { return _timeSavedPerTask; }
-            set
-            {
-                var valueDidChange = SetProperty(ref _timeSavedPerTask, value);
-                if (valueDidChange)
-                {
-                    MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
-                }
-            }
-        }
+		private int _timeSavedPerTask;
+		public int TimeSavedPerTask
+		{
+			get { return _timeSavedPerTask; }
+			set
+			{
+				var valueDidChange = SetProperty(ref _timeSavedPerTask, value);
+				if (valueDidChange)
+				{
+					MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
+				}
+			}
+		}
 
-        private string _taskFrequencyLabelText;
-        public string TaskFrequencyLabelText
-        {
-            get { return _taskFrequencyLabelText; }
-            set { SetProperty(ref _taskFrequencyLabelText, value); }
-        }
+		private string _taskFrequencyLabelText;
+		public string TaskFrequencyLabelText
+		{
+			get { return _taskFrequencyLabelText; }
+			set { SetProperty(ref _taskFrequencyLabelText, value); }
+		}
 
-        private int _taskFrequency;
-        public int TaskFrequency
-        {
-            get { return _taskFrequency; }
-            set
-            {
-                var valueDidChange = SetProperty(ref _taskFrequency, value);
-                if (valueDidChange)
-                {
-                    MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
-                }
-            }
-        }
+		private int _taskFrequency;
+		public int TaskFrequency
+		{
+			get { return _taskFrequency; }
+			set
+			{
+				var valueDidChange = SetProperty(ref _taskFrequency, value);
+				if (valueDidChange)
+				{
+					MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
+				}
+			}
+		}
 
-        private string _taskLifetimeLabelText;
-        public string TaskLifetimeLabelText
-        {
-            get { return _taskLifetimeLabelText; }
-            set { SetProperty(ref _taskLifetimeLabelText, value); }
-        }
+		private string _taskLifetimeLabelText;
+		public string TaskLifetimeLabelText
+		{
+			get { return _taskLifetimeLabelText; }
+			set { SetProperty(ref _taskLifetimeLabelText, value); }
+		}
 
-        private int _taskLifetime;
-        public int TaskLifetime
-        {
-            get { return _taskLifetime; }
-            set
-            {
-                var valueDidChange = SetProperty(ref _taskLifetime, value);
-                if (valueDidChange)
-                {
-                    MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
-                }
-            }
-        }
+		private int _taskLifetime;
+		public int TaskLifetime
+		{
+			get { return _taskLifetime; }
+			set
+			{
+				var valueDidChange = SetProperty(ref _taskLifetime, value);
+				if (valueDidChange)
+				{
+					MaxTimeToAllot = GetMaxTimeToAllotInMinutes();
+				}
+			}
+		}
 
-        private string _maxTimeToAllotLabelText;
-        public string MaxTimeToAllotLabelText
-        {
-            get { return _maxTimeToAllotLabelText; }
-            set { SetProperty(ref _maxTimeToAllotLabelText, value); }
-        }
+		private string _maxTimeToAllotLabelText;
+		public string MaxTimeToAllotLabelText
+		{
+			get { return _maxTimeToAllotLabelText; }
+			set { SetProperty(ref _maxTimeToAllotLabelText, value); }
+		}
 
-        private double _maxTimeToAllot;
-        public double MaxTimeToAllot
-        {
-            get { return _maxTimeToAllot; }
-            set { SetProperty(ref _maxTimeToAllot, value); }
-        }
+		private TimeSpan _maxTimeToAllot;
+		public TimeSpan MaxTimeToAllot
+		{
+			get { return _maxTimeToAllot; }
+			set { SetProperty(ref _maxTimeToAllot, value); }
+		}
 
-        private ObservableCollection<string> _unitsOfTime;
-        public ObservableCollection<string> UnitsOfTime
-        {
-            get { return _unitsOfTime; }
-            set { SetProperty(ref _unitsOfTime, value); }
-        }
+		private ObservableCollection<string> _frequencyUnitsOfTime;
+		public ObservableCollection<string> FrequencyUnitsOfTime
+		{
+			get { return _frequencyUnitsOfTime; }
+			set { SetProperty(ref _frequencyUnitsOfTime, value); }
+		}
 
-        private string _selectedFrequencyTimeUnit;
-        public string SelectedFrequencyTimeUnit
-        {
-            get { return _selectedFrequencyTimeUnit; }
-            set { SetProperty(ref _selectedFrequencyTimeUnit, value); }
-        }
+		private ObservableCollection<string> _unitsOfTime;
+		public ObservableCollection<string> UnitsOfTime
+		{
+			get { return _unitsOfTime; }
+			set { SetProperty(ref _unitsOfTime, value); }
+		}
 
-        private string _selectedTaskLifetimeTimeUnit;
-        public string SelectedTaskLifetimeTimeUnit
-        {
-            get { return _selectedTaskLifetimeTimeUnit; }
-            set { SetProperty(ref _selectedTaskLifetimeTimeUnit, value); }
-        }
+		private string _selectedFrequencyTimeUnit;
+		public string SelectedFrequencyTimeUnit
+		{
+			get { return _selectedFrequencyTimeUnit; }
+			set { SetProperty(ref _selectedFrequencyTimeUnit, value); }
+		}
 
-        public MainPageViewModel(ITimeSavingsCalculator timeSavingsCalculator)
-        {
-            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(MainPageViewModel)}:  ctor");
+		private string _selectedTaskLifetimeTimeUnit;
+		public string SelectedTaskLifetimeTimeUnit
+		{
+			get { return _selectedTaskLifetimeTimeUnit; }
+			set { SetProperty(ref _selectedTaskLifetimeTimeUnit, value); }
+		}
 
-            _timeSavingsCalculator = timeSavingsCalculator;
+		public MainPageViewModel(ITimeSavingsCalculator timeSavingsCalculator)
+		{
+			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(MainPageViewModel)}:  ctor");
 
-            UnitsOfTime = new ObservableCollection<string>
-            {
-                "per day", "per week", "per month", "per year"
-            };
+			_timeSavingsCalculator = timeSavingsCalculator;
 
-            Title = "EfficienSee";
-            TimeSavedLabelText = "By how many minutes could you shorten your task, given some effort to make some part of it automated, or more efficient?";
-            TaskFrequencyLabelText = "How frequently do you it?";
-            TaskLifetimeLabelText = "How long will you keep performing this task?";
-            MaxTimeToAllotLabelText = "You can spend this much time before spending more time than you will save:";
-        }
+			FrequencyUnitsOfTime = new ObservableCollection<string>
+			{
+				"per day", "per week", "per month", "per year"
+			};
 
-        ~MainPageViewModel()
-        {
-            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(MainPageViewModel)}:  dtor");
-        }
+			UnitsOfTime = new ObservableCollection<string>{
+				"days","weeks","months","years"
+			};
 
-        #region INavigatingAware
 
-        public void OnNavigatingTo(NavigationParameters parameters)
-        {
-            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNavigatingTo)}");
-        }
 
-        #endregion End INavigatingAware
+			Title = "EfficienSee";
+			TimeSavedLabelText = "By how many minutes could you shorten your task, given some effort to make some part of it automated, or more efficient?";
+			TaskFrequencyLabelText = "How frequently do you it?";
+			TaskLifetimeLabelText = "How long will you keep performing this task?";
+			MaxTimeToAllotLabelText = "You can spend this much time before spending more time than you will save:";
+		}
 
-        #region IDestructible
+		~MainPageViewModel()
+		{
+			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(MainPageViewModel)}:  dtor");
+		}
 
-        public void Destroy()
-        {
-            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(Destroy)}");
-        }
+		#region INavigatingAware
 
-        #endregion End IDestructible
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNavigatingTo)}");
+		}
 
-        internal double GetMaxTimeToAllotInMinutes()
-        {
-            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(GetMaxTimeToAllotInMinutes)}");
-            
-            var maxTime = _timeSavingsCalculator.GetMaxTimeSaved(
-                TimeSpan.FromMinutes(TimeSavedPerTask), 
-                TaskFrequency, 
-                SelectedFrequencyTimeUnit, 
-                TaskLifetime,
-                SelectedTaskLifetimeTimeUnit);
+		#endregion End INavigatingAware
 
-            return maxTime.TotalMinutes;
-        }
-    }
+		#region IDestructible
+
+		public void Destroy()
+		{
+			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(Destroy)}");
+		}
+
+		#endregion End IDestructible
+
+		internal TimeSpan GetMaxTimeToAllotInMinutes()
+		{
+			var maxTime = _timeSavingsCalculator.GetMaxTimeSaved(
+				TimeSpan.FromMinutes(TimeSavedPerTask),
+				TaskFrequency,
+				SelectedFrequencyTimeUnit,
+				TaskLifetime,
+				SelectedTaskLifetimeTimeUnit);
+
+			return maxTime;
+		}
+	}
 }
 
---
+++ EfficienSee/Views/ValueConverters/TimeSpanToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EfficienSee.Views.ValueConverters
+{
+	public class TimeSpanToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			TimeSpan time;
+			string display = string.Empty;
+
+			if (value is TimeSpan == false)
+			{
+				display = "???";
+			}
+			else
+			{
+				time = (TimeSpan)value;
+				display = $"{time.Days} days {time.Hours} hours and {time.Minutes} minutes";
+			}
+
+			return display;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
